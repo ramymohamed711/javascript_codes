e2ad5af3e24904af2c3efc1b211b2b10687c2c24
ramymohamed711 <ramy711@yahoo.com>
2016-06-17 03:47:54 +0300

    Add files via upload

---
+++ 3-Global scope.js
@@ -0,0 +1,16 @@
+var firstName ;
+var lastName;
+var person = {
+firstName : 'John',
+lastName : 'Doe',
+getFullNameFunction : function() {
+/*
+The bug in the return function() that it doesn't detect the firstName and lastName variables in it's outer scope "getFullNameFunction() function scope" .
+to repair it define new global variables firstName and lastName and assign to them the values of the variables firstName and lastName in
+person() scope.
+*/
+ firstName =  this.firstName;
+  lastName  =  this.lastName;
+return (function(){ return  this.firstName + ' ' + this.lastName });
+}
+};
